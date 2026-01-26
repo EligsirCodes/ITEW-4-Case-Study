85a43eecf54ce8c5b93b31144a13da895265060f
Eligsir <eligsircodes@gmail.com>
2026-01-26 09:51:41 +0800

    NavRoutes Setup

---
+++ app/src/main/java/com/example/itew4_casestudy/navigation/NavRoutes.kt
@@ -0,0 +1,11 @@
+package com.example.itew4_casestudy.navigation
+
+object Routes {
+    const val LOGIN_SCREEN = "login_screen"
+    const val REGISTRATION_SCREEN = "registration_screen"
+    const val DASHBOARD_SCREEN = "dashboard_screen"
+    const val INFORMATION_SCREEN = "information_screen"
+    const val TASK_SCREEN = "task_screen"
+    const val ANNOUNCEMENT_SCREEN = "announcement_screen"
+    const val SETTINGS_SCREEN = "settings_screen"
+}
