d2000a9c2c0313fd0ecdc6ae76140a2b6ac7d1e9
Joronski <joronrecio@yahoo.com>
2026-02-01 21:20:56 +0800

    feat: initialize LoginScreen state and shared preferences

--- app/src/main/java/com/example/itew4_casestudy/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/screens/LoginScreen.kt
@@ -1,9 +1,26 @@
 package com.example.itew4_casestudy.presentation.screens
 
+import android.content.Context
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @Composable
 fun LoginScreen(navController: NavController) {
+    val context = LocalContext.current
+    val sharedPrefs = context.getSharedPreferences(
+        "user_session",
+        Context.MODE_PRIVATE
+    )
+
+    val scrollState = rememberScrollState()
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
 
 }
