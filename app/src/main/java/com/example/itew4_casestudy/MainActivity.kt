a294e5c6737d18074ae384571996a5d599b0cc3f
Eligsir <eligsircodes@gmail.com>
2026-02-01 17:27:54 +0800

    MainActivity Update

--- app/src/main/java/com/example/itew4_casestudy/MainActivity.kt
+++ app/src/main/java/com/example/itew4_casestudy/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.itew4_casestudy.navigation.AppNavHost
 import com.example.itew4_casestudy.ui.theme.ITEW4CaseStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ITEW4CaseStudyTheme {
+                val navController = rememberNavController()
 
+                AppNavHost(navController)
             }
         }
     }
