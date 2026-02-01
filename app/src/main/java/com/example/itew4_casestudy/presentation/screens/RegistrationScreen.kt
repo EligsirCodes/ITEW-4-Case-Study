66c19e61d627690fc13d0679dbe8451421cf75a5
Quitorio <marcalvinq@gmail.com>
2026-02-01 18:45:33 +0800

    Initialize RegistrationScreen states and main layout structure

--- app/src/main/java/com/example/itew4_casestudy/presentation/screens/RegistrationScreen.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/screens/RegistrationScreen.kt
@@ -1,9 +1,42 @@
 package com.example.itew4_casestudy.presentation.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.itew4_casestudy.R
+import com.example.itew4_casestudy.navigation.Routes
 
 @Composable
 fun RegistrationScreen(navController: NavController) {
+    val scrollState = rememberScrollState()
+    var userName by rememberSaveable { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var confirmPassword by rememberSaveable { mutableStateOf("") }
 
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {}
