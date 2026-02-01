c0512d8b85835c0b9201f0ec2f21e66b618bc141
johnlesterpenafiel5-mrellie <johnlesterpenafiel5@gmail.com>
2026-02-01 18:15:15 +0800

    imports

--- app/src/main/java/com/example/itew4_casestudy/presentation/screens/TasksScreen.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/screens/TasksScreen.kt
@@ -1,7 +1,17 @@
 package com.example.itew4_casestudy.presentation.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Task
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
+import com.example.itew4_casestudy.navigation.Routes
+import kotlinx.coroutines.launch
 
 @Composable
 fun TaskScreen(navController: NavController) {
