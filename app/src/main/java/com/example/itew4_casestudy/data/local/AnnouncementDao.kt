efb331d9d19b8c4f431305a02a20978b6785b173
Eligsir <eligsircodes@gmail.com>
2026-02-25 16:34:18 +0800

    Personal Commit

--- app/src/main/java/com/example/itew4_casestudy/data/local/AnnouncementDao.kt
+++ app/src/main/java/com/example/itew4_casestudy/data/local/AnnouncementDao.kt
@@ -1,2 +1,34 @@
 package com.example.itew4_casestudy.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface AnnouncementDao {
+    // Insert announcement
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAnnouncement(announcement: AnnouncementEntity)
+
+    // Get all announcements
+    @Query("SELECT * FROM announcements ORDER BY id DESC")
+    suspend fun getAllAnnouncements(): List<AnnouncementEntity>
+
+    // Get all READ announcements
+    @Query("SELECT * FROM announcements WHERE isRead = 1 ORDER BY id DESC")
+    suspend fun getReadAnnouncements(): List<AnnouncementEntity>
+
+    // Get all UNREAD announcements
+    @Query("SELECT * FROM announcements WHERE isRead = 0 ORDER BY id DESC")
+    suspend fun getUnreadAnnouncements(): List<AnnouncementEntity>
+
+    // Mark announcement as read
+    @Query("UPDATE announcements SET isRead = 1 WHERE id = :announcementId")
+    suspend fun markAsRead(announcementId: Int)
+
+    // Update whole object if needed
+    @Update
+    suspend fun updateAnnouncement(announcement: AnnouncementEntity)
+}
