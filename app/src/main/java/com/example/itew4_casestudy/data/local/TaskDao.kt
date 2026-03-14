2cce13c9a3d374fe20f4415c39598781ef3edd77
ann <rabanotrishaann8@gmail.com>
2026-03-14 21:33:41 +0800

    Add TaskDao interface

--- app/src/main/java/com/example/itew4_casestudy/data/local/TaskDao.kt
+++ app/src/main/java/com/example/itew4_casestudy/data/local/TaskDao.kt
@@ -1,2 +1,13 @@
 package com.example.itew4_casestudy.data.local
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: TaskEntity) }
