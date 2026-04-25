e7c617cc12714798e93f2fe50873b16d3244ce3c
ann <rabanotrishaann8@gmail.com>
2026-04-26 03:21:57 +0800

    Create ThemeViewModel class

--- app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/ThemeViewModel.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/ThemeViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.itew4_casestudy.presentation.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ThemeViewModel(application: Application) : AndroidViewModel(application) {
+
+}
