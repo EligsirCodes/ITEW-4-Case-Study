04330f3a3d34b8c844661924d3e8d73da077559c
johnlesterpenafiel5-mrellie <johnlesterpenafiel5@gmail.com>
2026-04-25 23:09:07 +0800

    Adding AnnouncementFilter enum

--- app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/AnnouncementViewModel.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/AnnouncementViewModel.kt
@@ -2,7 +2,7 @@ package com.example.itew4_casestudy.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 
-
+enum class AnnouncementFilter { ALL, READ, UNREAD }
 class AnnouncementViewModel(
     private val repository: AnnouncementRepository = AnnouncementRepository()
 ) : ViewModel() {
