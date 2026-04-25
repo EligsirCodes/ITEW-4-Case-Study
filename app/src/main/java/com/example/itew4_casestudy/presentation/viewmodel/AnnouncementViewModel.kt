c5268b97b24ec12e73594c416fcce714c46807b6
johnlesterpenafiel5-mrellie <johnlesterpenafiel5@gmail.com>
2026-04-25 23:15:57 +0800

    Setting up Compose state for announcements and filter

--- app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/AnnouncementViewModel.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/AnnouncementViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.itew4_casestudy.presentation.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 enum class AnnouncementFilter { ALL, READ, UNREAD }
 class AnnouncementViewModel(
     private val repository: AnnouncementRepository = AnnouncementRepository()
 ) : ViewModel() {
+    private var allAnnouncements = listOf<AnnouncementModel>()
+    var announcements by mutableStateOf<List<AnnouncementModel>>(emptyList())
+        private set
+    var currentFilter by mutableStateOf(AnnouncementFilter.ALL)
+        private set
 
 }
