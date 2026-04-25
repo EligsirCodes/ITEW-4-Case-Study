185c1fe3c4cf66522b1b525ce171068ee11a0b07
EligsirCodes <eligsircodes@gmail.com>
2026-04-25 20:53:38 +0800

    Develop: Model Implementations for Firebase Integration

---
+++ app/src/main/java/com/example/itew4_casestudy/domain/model/UserModel.kt
@@ -0,0 +1,8 @@
+package com.example.itew4_casestudy.domain.model
+
+data class UserModel(
+    val uid: String = "",
+    val name: String = "",
+    val email: String = "",
+    val role: String = "Student"
+)
