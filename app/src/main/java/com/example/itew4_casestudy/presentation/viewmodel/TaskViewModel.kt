a18b9ef4c1fecb58efef477a4ede3ceaae371c71
Joronski <joronrecio@yahoo.com>
2026-04-26 00:54:06 +0800

    Implementing FirebaseAuth user tracking

--- app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/TaskViewModel.kt
@@ -6,10 +6,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.example.itew4_casestudy.domain.model.TaskModel
 import com.example.itew4_casestudy.data.repository.TaskRepository
+import com.google.firebase.auth.FirebaseAuth
 
 class TaskViewModel(
     private val repository: TaskRepository
 ) : ViewModel() {
     var tasks by mutableStateOf<List<TaskModel>>(emptyList())
         private set
+
+    private val currentUserId: String
+        get() = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
+    init {
+        FirebaseAuth.getInstance().addAuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                // will listen later
+            } else {
+                tasks = emptyList()
+            }
+        }
+    }
 }
