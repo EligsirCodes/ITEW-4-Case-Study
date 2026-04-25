734ad10231fb2eba8fc3c65fd8b7c9cce10b428a
Joronski <joronrecio@yahoo.com>
2026-04-26 00:52:03 +0800

    Added tasks state management

--- app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/itew4_casestudy/presentation/viewmodel/TaskViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.itew4_casestudy.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.itew4_casestudy.domain.model.TaskModel
 import com.example.itew4_casestudy.data.repository.TaskRepository
 
 class TaskViewModel(
     private val repository: TaskRepository
 ) : ViewModel() {
-
+    var tasks by mutableStateOf<List<TaskModel>>(emptyList())
+        private set
 }
